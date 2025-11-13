8479617e75a1f22bd37718b90245843a9c9f7f32
thenishantnayan <nayannishant999@gmail.com>
2025-11-13 13:02:43 +0530

    adding all files and folders

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I’m <span className="purple">Nishant Nayan</span>{" "}
+            from <span className="purple">Delhi, India</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            I’m Studying{" "}
+            <span className="purple"></span> B.Tech at{" "}
+            <span className="purple">KIET Group of Institution</span>.
+            <br />in Computer Science Branch{" "}
+            {/* <span className="purple">Mathematics and Computing</span> from{" "}
+            <span className="purple">BIT Mesra</span>. */}
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
@@ -38,7 +38,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Nishant</footer>
         </blockquote>
       </Card.Body>
     </Card>
